e7f9a975907b97afa3b44df80a9b95e7b05386aa
tshirt519 <tshirt519@gmail.com>
2021-07-26 17:22:59 +0900

    Scrollボタンひとまず表示

--- components/Top/index.jsx
+++ components/Top/index.jsx
@@ -4,14 +4,19 @@ import Image from "next/image";
 export const Top = () => {
   return (
     <section className={styles.section}>
-      <Image className={styles.Image}
+      <Image
         src="/TV-6.jpg"
         alt="top visual"
         width={1680}
         height={1074}
-        objectPosition="center" 
+        objectPosition="center"
         objectFit="cover"
       ></Image>
+
+      <div className={styles.div}>
+        <p>SCROLL</p>
+        <p>↓</p>
+      </div>
     </section>
   );
 };
