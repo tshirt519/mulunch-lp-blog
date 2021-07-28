f70f9b0667da1baa5704b6e084424f26d62557e2
tshirt519 <tshirt519@gmail.com>
2021-07-28 18:07:53 +0900

    ✨ topicsの要素と大まかなレイアウト

--- components/Topic/index.jsx
+++ components/Topic/index.jsx
@@ -1,10 +1,47 @@
 import styles from "../Topic/Topic.module.css";
-
+import Image from "next/image";
 
 export const Topic = () => {
+  const topics = [
+    {
+      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+    },
+    {
+      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+    },
+    {
+      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+    },
+    {
+      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+    },
+  ];
+
   return (
     <section className={styles.section}>
-      Topic
+      <p>最新のお知らせ</p>
+      <h3>TOPICS</h3>
+      <div>
+        <ul className={styles.ul}>
+          <button>＜</button>
+          {topics.map((topic, i) => (
+            <li key={i}>
+              <Image
+                src={topic.url}
+                width={300}
+                height={300}
+                alt="topics image"
+              />
+              <p>{topic.title}</p>
+            </li>
+          ))}
+          <button>＞</button>
+        </ul>
+      </div>
     </section>
   );
-}
+};
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['mulunchimage.s3-ap-northeast-1.amazonaws.com'],
+  },
 }
