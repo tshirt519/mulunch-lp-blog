b16dccf3ac6351d9e5d7c4aef9ca56dc8fd69194
tshirt519 <tshirt519@gmail.com>
2021-07-28 14:33:46 +0900

    scrollbutton実装

--- components/Top/index.jsx
+++ components/Top/index.jsx
@@ -1,5 +1,6 @@
 import styles from "../Top/Top.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Top = () => {
   return (
@@ -14,8 +15,19 @@ export const Top = () => {
       ></Image>
 
       <div className={styles.div}>
-        <p>SCROLL</p>
-        <p>↓</p>
+        <Link href="#">
+          <a>
+            <p>SCROLL</p>
+            <svg
+              className={styles.svg}
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
+            </svg>
+          </a>
+        </Link>
       </div>
     </section>
   );
