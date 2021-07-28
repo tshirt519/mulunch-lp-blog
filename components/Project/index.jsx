9f661fdbea4dec71d7ba32cceb7a4d2e3833ac84
tshirt519 <tshirt519@gmail.com>
2021-07-28 16:59:53 +0900

    grid 仮レイアウト

--- components/Project/index.jsx
+++ components/Project/index.jsx
@@ -1,5 +1,6 @@
 import styles from "../Project/Project.module.css";
 import Image from "next/image";
+import { style } from "dom-helpers";
 
 export const Project = () => {
   return (
@@ -18,37 +19,51 @@ export const Project = () => {
         <p>他にむらとつながりながら、田舎体験や観光などを楽しんでいます。</p>
       </div>
 
-      <div className={styles.grid}>
-        <Image className={styles.upper1}
-          src="/actimg1-5.png"
-          alt="project image"
-          width={100}
-          height={100}
-        />
-        <Image className={styles.upper2}
-          src="/actimg2-2.png"
-          alt="project image"
-          width={100}
-          height={100}
-        />
-        <Image className={styles.upper3}
-          src="/actimg3-4.png"
-          alt="project image"
-          width={100}
-          height={100}
-        />
-        <Image className={styles.lower1}
-          src="/actimg4-2.png"
-          alt="project image"
-          width={100}
-          height={100}
-        />
-        <Image className={styles.lower2}
-          src="/actimg5-2.png"
-          alt="project image"
-          width={100}
-          height={100}
-        />
+      <div className={styles.grid_upper}>
+        <div className={styles.upper1}>
+          <Image
+            src="/actimg1-5.png"
+            alt="project image"
+            width={300}
+            height={300}
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+        <div className={styles.upper2}>
+          <Image
+            src="/actimg2-2.png"
+            alt="project image"
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className={styles.upper3}>
+          <Image
+            src="/actimg3-4.png"
+            alt="project image"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+      <div className={styles.grid_lower}>
+        <div className={styles.lower1}>
+          <Image
+            src="/actimg4-2.png"
+            alt="project image"
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className={styles.lower2}>
+          <Image
+            src="/actimg5-2.png"
+            alt="project image"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
     </section>
   );
