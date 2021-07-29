091f24250d87beee0b61498c88fa008b6c8d2f82
tshirt519 <tshirt519@gmail.com>
2021-07-29 16:11:31 +0900

    ✨ topicsのカルーセル処理実装

--- components/Topic/index.jsx
+++ components/Topic/index.jsx
@@ -1,25 +1,61 @@
 import styles from "../Topic/Topic.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
-export const Topic = () => {
+export const Topic = ({ topic }) => {
   const topics = [
     {
-      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
-      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title1: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      image1:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title2: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      image2:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title3: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      image3:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title4: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
+      image4:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
     },
     {
-      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
-      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title1: "スナックむらむすび、始まります",
+      image1:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/スナックむらむすび、始まります_1.jpg",
+      title2: "スナックむらむすび、始まります",
+      image2:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/スナックむらむすび、始まります_1.jpg",
+      title3: "スナックむらむすび、始まります",
+      image3:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/スナックむらむすび、始まります_1.jpg",
+      title4: "スナックむらむすび、始まります",
+      image4:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/スナックむらむすび、始まります_1.jpg",
     },
     {
-      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
-      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
-    },
-    {
-      title: "地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。",
-      url: "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/地域と人を繋いでいく。学生が仕掛ける「むらんち」が描く未来。_1.jpg",
+      title1: "神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店",
+      image1:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店_1.jpg",
+      title2: "神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店",
+      image2:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店_1.jpg",
+      title3: "神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店",
+      image3:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店_1.jpg",
+      title4: "神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店",
+      image4:
+        "https://mulunchimage.s3-ap-northeast-1.amazonaws.com/posts/神楽坂「むらむすび」営業再開見通せず　東成瀬村アンテナ店_1.jpg",
     },
   ];
+
+  const [current, setCurrent] = useState(0);
+  const length = topics.length;
+  const prevSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const nextSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
 
   return (
     <section className={styles.section}>
@@ -27,19 +63,56 @@ export const Topic = () => {
       <h3>TOPICS</h3>
       <div>
         <ul className={styles.ul}>
-          <button>＜</button>
-          {topics.map((topic, i) => (
-            <li key={i}>
-              <Image
-                src={topic.url}
-                width={300}
-                height={300}
-                alt="topics image"
-              />
-              <p>{topic.title}</p>
+          <button className={styles.next} onClick={prevSlide}>
+            ＜
+          </button>
+          {topics.map((topic, index) => (
+            <li key={index}>
+              {index === current && (
+                <div className={styles.ul}>
+                  <article>
+                    <Image
+                      src={topic.image1}
+                      width={300}
+                      height={300}
+                      alt="topics image"
+                    />
+                    <p>{topic.title1}</p>
+                  </article>
+                  <article>
+                    <Image
+                      src={topic.image2}
+                      width={300}
+                      height={300}
+                      alt="topics image"
+                    />
+                    <p>{topic.title2}</p>
+                  </article>
+                  <article>
+                    <Image
+                      src={topic.image3}
+                      width={300}
+                      height={300}
+                      alt="topics image"
+                    />
+                    <p>{topic.title3}</p>
+                  </article>
+                  <article>
+                    <Image
+                      src={topic.image4}
+                      width={300}
+                      height={300}
+                      alt="topics image"
+                    />
+                    <p>{topic.title4}</p>
+                  </article>
+                </div>
+              )}
             </li>
           ))}
-          <button>＞</button>
+          <button className={styles.next} onClick={nextSlide}>
+            ＞
+          </button>
         </ul>
       </div>
     </section>
