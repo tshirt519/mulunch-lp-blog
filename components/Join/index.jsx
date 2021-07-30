eb08cb5e635dda6c9a2ed3ca0d9ffd9887132e03
tshirt519 <tshirt519@gmail.com>
2021-07-30 18:48:32 +0900

    ✨ joinセクション追加　全体の大まかなレイアウトは完成

--- components/Join/index.jsx
+++ components/Join/index.jsx
@@ -1,10 +1,60 @@
 import styles from "../Join/Join.module.css";
-
+import Image from "next/image";
+import { style } from "dom-helpers";
 
 export const Join = () => {
   return (
     <section className={styles.section}>
-      Join
+      <div className={styles.heading}>
+        <div>
+          <h2>JOIN</h2>
+          <p>参加してみよう！</p>
+        </div>
+        <div>
+          <Image
+            src="/join-right-3.png"
+            alt="join description"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
+      <div className={styles.description}>
+        <h3>「村」をむすぼう</h3>
+        <p>
+          <span className={styles.mulunch_font}>むらんち</span>
+          では、一緒にむらをむすぶ仲間を募集しています。
+        </p>
+        <p>
+          いろんな興味・関心を持ったメンバーの関わりのなかで活動がうまれています。
+        </p>
+        <p>少しでも興味がある方はお気軽にお問い合わせくださいね！</p>
+      </div>
+
+      <div className={styles.flow}>
+        <Image
+          src="/join-flow1.png"
+          alt="join flow"
+          width={1200}
+          height={800}
+        />
+      </div>
+
+      <form className={styles.form} action="mailto:muramusubi.lunch@gmail.com" method="POST">
+        <div>
+          <input type="text" name="shimei" placeholder="お名前" />
+          <input type="email" name="email" placeholder="メールアドレス" />
+        </div>
+        <textarea
+          name="contact"
+          cols="150"
+          rows="15"
+          placeholder="お問い合わせ内容"
+        ></textarea>
+        <input type="submit" value="送信" />
+      </form>
+
+      <div className={styles.transition}></div>
     </section>
   );
-}
+};
